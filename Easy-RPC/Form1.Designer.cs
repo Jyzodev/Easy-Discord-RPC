7f55684b4180a1548bbf6278f077e5be4231d083
Jyzodev <bazza.youssef13@gmail.com>
2022-10-15 12:21:22 +0100

    First and last one
    Made for fun
    Discord : jyzo.#6858

---
+++ Easy-RPC/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace Easy_RPC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.app_ID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.img_1_txt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.img_2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.img_1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.img_2_txt = new System.Windows.Forms.TextBox();
+            this.system_tray = new System.Windows.Forms.NotifyIcon(this.components);
+            this.btn1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn1_url = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.details = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.state = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Application ID";
+            // 
+            // app_ID
+            // 
+            this.app_ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.app_ID.Location = new System.Drawing.Point(160, 20);
+            this.app_ID.Name = "app_ID";
+            this.app_ID.Size = new System.Drawing.Size(173, 27);
+            this.app_ID.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Image 2 Text";
+            // 
+            // img_1_txt
+            // 
+            this.img_1_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.img_1_txt.Location = new System.Drawing.Point(160, 141);
+            this.img_1_txt.Name = "img_1_txt";
+            this.img_1_txt.Size = new System.Drawing.Size(173, 27);
+            this.img_1_txt.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Image 1 Text";
+            // 
+            // img_2
+            // 
+            this.img_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.img_2.Location = new System.Drawing.Point(160, 103);
+            this.img_2.Name = "img_2";
+            this.img_2.Size = new System.Drawing.Size(173, 27);
+            this.img_2.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(24, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 22);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Image 2";
+            // 
+            // img_1
+            // 
+            this.img_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.img_1.Location = new System.Drawing.Point(160, 59);
+            this.img_1.Name = "img_1";
+            this.img_1.Size = new System.Drawing.Size(173, 27);
+            this.img_1.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(24, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 22);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Large Image";
+            // 
+            // img_2_txt
+            // 
+            this.img_2_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.img_2_txt.Location = new System.Drawing.Point(160, 174);
+            this.img_2_txt.Name = "img_2_txt";
+            this.img_2_txt.Size = new System.Drawing.Size(173, 27);
+            this.img_2_txt.TabIndex = 3;
+            // 
+            // system_tray
+            // 
+            this.system_tray.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.system_tray.BalloonTipText = "idfkkk";
+            this.system_tray.BalloonTipTitle = "xDDD";
+            this.system_tray.Icon = ((System.Drawing.Icon)(resources.GetObject("system_tray.Icon")));
+            this.system_tray.Text = "Discord RPC";
+            this.system_tray.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.system_tray_MouseDoubleClick);
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(160, 217);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(173, 27);
+            this.btn1.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(24, 217);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 22);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Button 1";
+            // 
+            // btn1_url
+            // 
+            this.btn1_url.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1_url.Location = new System.Drawing.Point(160, 255);
+            this.btn1_url.Name = "btn1_url";
+            this.btn1_url.Size = new System.Drawing.Size(173, 27);
+            this.btn1_url.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(24, 255);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(118, 22);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "URL Button 1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(45, 310);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(404, 60);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Enable RPC";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // details
+            // 
+            this.details.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.details.Location = new System.Drawing.Point(530, 20);
+            this.details.Name = "details";
+            this.details.Size = new System.Drawing.Size(173, 27);
+            this.details.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(424, 25);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 22);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Details";
+            // 
+            // state
+            // 
+            this.state.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.state.Location = new System.Drawing.Point(530, 59);
+            this.state.Name = "state";
+            this.state.Size = new System.Drawing.Size(173, 27);
+            this.state.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(424, 64);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(52, 22);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "State";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 389);
+            this.Controls.Add(this.state);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.details);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn1_url);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.img_1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.img_2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.img_1_txt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.img_2_txt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.app_ID);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Easy Discord RPC (C#)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox app_ID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox img_1_txt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox img_2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox img_1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox img_2_txt;
+        private System.Windows.Forms.NotifyIcon system_tray;
+        private System.Windows.Forms.TextBox btn1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox btn1_url;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox details;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox state;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Easy-RPC/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DiscordRPC;
+using Button = DiscordRPC.Button;
+
+namespace Easy_RPC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                system_tray.Visible = true;
+            }
+        }
+
+        private void system_tray_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            system_tray.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (app_ID.Text != String.Empty)
+            {
+                if (details.Text != String.Empty || state.Text != String.Empty || img_1.Text != String.Empty)
+                {
+                    DiscordRpcClient client = new DiscordRpcClient(app_ID.Text);
+                    client.Initialize();
+
+                    RichPresence presence = new RichPresence();
+                    presence.Details = details.Text;
+                    presence.State = state.Text;
+                    presence.Assets = new Assets()
+                    {
+                        LargeImageKey = img_1.Text,
+                        LargeImageText = img_1_txt.Text,
+                        SmallImageKey = img_2.Text,
+                        SmallImageText = img_2_txt.Text
+                    };
+                    presence.Buttons = new Button[]
+                            {
+                                new Button() { Label = btn1.Text, Url = btn1_url.Text }
+                            };
+                    client.SetPresence(presence);
+                }
+
+
+            }
+            else
+            {
+                MessageBox.Show("DONT YOU HAVE AN ID??");
+            }
+        }
+    }
+}
